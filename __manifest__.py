02dbb01c965b6ed04ad3fcf7fd4f920c73c4bb6c
workspace <vipul.shrivastav@in2ittech.com>
2026-02-10 12:33:00 +0530

    Mnay2one xml changes on data file

--- __manifest__.py
+++ __manifest__.py
@@ -10,6 +10,7 @@
         "security/ir.model.access.csv",
         "data/owner.csv",
         # "data/estate.property.csv",
+        "data/owner.xml",
         "views/estate_property.xml",
         "views/owner.xml",
         "views/enquiry.xml",
